9fe83f9b9b61b1eb383e99088d1a0b77a0338256
Nelson Hernández <nelsonhdz007@gmail.com>
2024-09-14 17:12:01 -0600

    datos quemados para pruebas

--- Program.cs
+++ Program.cs
@@ -26,43 +26,42 @@ namespace ChallengeOneLibraryDAS01
 
             book.Id = 1000;
             book.Title = "el principito";
-            book.Author = "nelson  hdz";
+            book.Author = "Antoine de Saint-Exupéry";
             book.Stock = 3;
-            book.PublicationDate = new DateTime(2017, 9, 15);
-            book.PortraitUrl = "www.josejose.img";
-            book.Editorial = "bacacho";
-
+            book.PublicationDate = new DateTime(1954, 9, 15);
+            book.PortraitUrl = "https://m.media-amazon.com/images/I/71AVK5VIAzL._AC_UF1000,1000_QL80_.jpg";
+            book.Editorial = "Roble";
 
             book4.Id = 1004;
-            book4.Title = "cuentos del ayeryhoy";
-            book4.Author = "nelson  hdz";
+            book4.Title = "el señor de los anillos";
+            book4.Author = "J. R. R. Tolkien";
             book4.Stock = 9;
-            book4.PublicationDate = new DateTime(2015, 9, 15);
-            book4.PortraitUrl = "www.nelsonhdz";
-            book4.Editorial = "bacacho";
+            book4.PublicationDate = new DateTime(1954, 9, 15);
+            book4.PortraitUrl = "https://m.media-amazon.com/images/I/71oVTrHAylL._AC_UF894,1000_QL80_.jpg";
+            book4.Editorial = "Roble";
 
             book2.Id = 1002;
-            book2.Title = "el principe de la cancion";
-            book2.Author = "jose jose";
+            book2.Title = "el extranjero";
+            book2.Author = "Albert Camus";
             book2.Stock = 6;
-            book2.PublicationDate = new DateTime(2015, 9, 15);
-            book2.PortraitUrl = "www.josejose.img";
-            book2.Editorial = "bacacho";
+            book2.PublicationDate = new DateTime(1942, 9, 15);
+            book2.PortraitUrl = "https://images.cdn2.buscalibre.com/fit-in/360x360/0d/30/0d30665fd8b3d4e5860cd17a4d9fddcd.jpg";
+            book2.Editorial = "Roble";
 
             book3.Id = 1003;
-            book3.Title = "el viaje de chihiro";
-            book3.Author = "miyazaki";
+            book3.Title = "el señor de los anillos - El retorno del rey";
+            book3.Author = "J. R. R. Tolkien";
             book3.Stock = 10;
-            book3.PublicationDate = new DateTime(2016, 9, 15);
-            book3.PortraitUrl = "www.josejose.img";
-            book3.Editorial = "bacacho";
+            book3.PublicationDate = new DateTime(1956, 9, 15);
+            book3.PortraitUrl = "www.urlincorrecta.prueba.com";
+            book3.Editorial = "Roble";
 
 
             user.Id = 1001;
-            user.Name = "rodrigo lemus";
+            user.Name = "Rodrigo Lemus";
 
             user2.Id = 1010;
-            user2.Name = "eduardo lemus";
+            user2.Name = "Nelson Hernández";
 
             userdb.Insert(user);
             userdb.Insert(user2);
